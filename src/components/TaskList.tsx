655444b5cdec22ed2cc8e3945ccb5e8d0278e665
Paulo Freire <paulofreire@frwk.com.br>
2022-12-30 16:02:22 -0300

    add tasks list and css properties

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -11,6 +11,90 @@ function TaskList() {
         ></input>
         <button type="submit">Criar</button>
       </div>
+
+      <main>
+        <div className="tasksTrack">
+          <div className="createdTasks">
+            <p id="tasksCounterName">Tarefas criadas</p>
+            <p id="tasksCounter">3</p>
+          </div>
+          <div className="doneTasks">
+            <p id="tasksCounterName">Concluídas</p>
+            <p id="tasksCounter">0 de 3</p>
+          </div>
+        </div>
+        <ul>
+          {/* {tasks.map(task => ( */}
+          <li key={1}>
+            <div className="task">
+              <label className="checkbox-container">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  readOnly
+                  // checked={task.isComplete}
+                  onClick={() => "task.id"}
+                />
+              </label>
+              <p>{"task.title"}</p>
+            </div>
+
+            {/* <button
+              type="button"
+              data-testid="remove-task-button"
+              onClick={() => "click"}
+            >
+              <FiTrash size={16}/>
+            </button> */}
+          </li>
+          <li key={2}>
+            <div className="task">
+              <label className="checkbox-container">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  readOnly
+                  // checked={task.isComplete}
+                  onClick={() => "task.id"}
+                />
+                <span className="checkmark"></span>
+              </label>
+              <p>{"task.title"}</p>
+            </div>
+
+            {/* <button
+              type="button"
+              data-testid="remove-task-button"
+              onClick={() => "click"}
+            >
+              <FiTrash size={16}/>
+            </button> */}
+          </li>
+          <li key={3}>
+            <div className="task">
+              <label className="checkbox-container">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  readOnly
+                  // checked={task.isComplete}
+                  onClick={() => "task.id"}
+                />
+                <span className="checkmark"></span>
+              </label>
+              <p>{"task.title"}</p>
+            </div>
+
+            {/* <button
+              type="button"
+              data-testid="remove-task-button"
+              onClick={() => "click"}
+            >
+              <FiTrash size={16}/>
+            </button> */}
+          </li>
+        </ul>
+      </main>
     </section>
   );
 }
